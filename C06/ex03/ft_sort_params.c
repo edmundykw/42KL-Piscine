73d076fcb7c70a8d25efba71601dfc8bbd71e186
Edmund yap Keen wye <ekeen-wy@u91z01s03.42kl.edu.my>
2021-06-13 10:59:26 +0800

    C06

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekeen-wy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/06 12:21:41 by ekeen-wy          #+#    #+#             */
+/*   Updated: 2021/06/07 20:33:25 by ekeen-wy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include<unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	write(1, &str[i], j);
+	write(1, "\n", 1);
+}
+
+char	ft_check_str(char *str1, char *str2)
+{
+	int	i;
+
+	i = 0;
+	while (str1[i] != '\0' && str2[i] != '\0')
+	{
+		if (str1[i] != str2[i])
+		{
+			return (str1[i] - str2[i]);
+		}
+		i++;
+	}
+	return (str1[i] - str2[i]);
+}
+
+void	ft_sort_params(int	argc, char *arr[])
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_check_str(arr[i], arr[j]) > 0)
+			{
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	ft_sort_params(argc, argv);
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		i++;
+	}
+}
